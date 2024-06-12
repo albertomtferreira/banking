2904e60d9a4007034eccc84b6e013af54c5bf091
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2024-06-12 11:54:33 +0000

    create header and total balance box (00:42:02)

---
+++ components/ui/TotalBalanceBox.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const TotalBalanceBox = ({
+  accounts = [],
+  totalBanks,
+  totalCurrentBalance,
+}: TotalBalanceBoxProps) => {
+  return (
+    <section className="total-balance">
+      <div className="total-balance-chart">{/* Chart */}</div>
+      <div className="flex flex-col gap-6">
+        <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">Total Current Balance</p>
+          <p className="total-balance-amount flex-center gap-2">
+            {totalCurrentBalance}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TotalBalanceBox;
