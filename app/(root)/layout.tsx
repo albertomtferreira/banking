58760898a800a9961c5396fdb4b685dabcf78f1d
Alberto Ferreira <albertomtferreira@gmail.com>
2024-06-12 21:20:55 +0100

    add sidebar (01:05:58)

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName: "Alberto", lastName: "Tao"}
   return (
-<main>
-  SIDEBAR
-    {children}
-</main>
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn}/>  
+      {children}
+    </main>
   );
 }
