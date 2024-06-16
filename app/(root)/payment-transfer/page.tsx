221b66aea631095a106f23fc53e70f6381ba1cd3
Alberto Ferreira <albertomtferreira@gmail.com>
2024-06-16 15:05:43 +0100

    implement routing on sidebar

---
+++ app/(root)/payment-transfer/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Transfer = () => {
+  return (
+    <div>Transfer
+
+    </div>
+  )
+}
+
+export default Transfer
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransactionHistory = () => {
+  return (
+    <div>Transaction History
+
+    </div>
+  )
+}
+
+export default TransactionHistory
