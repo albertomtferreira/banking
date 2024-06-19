fef9b6738ae2e4a61ab8cb17af7a588d13722759
Alberto Ferreira <albertomtferreira@gmail.com>
2024-06-19 20:46:50 +0100

    fix doughnut chart to remove dummy data

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -1,24 +1,26 @@
 "use client"
 import React from 'react'
-import {Chart as ChartJS, ArcElement, Tooltip, Legend} from "chart.js"
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js"
 import { Doughnut } from 'react-chartjs-2';
-ChartJS.register(ArcElement,Tooltip,Legend)
+ChartJS.register(ArcElement, Tooltip, Legend)
 
 
-const DoughnutChart = ({accounts}: DoughnutChartProps) => {
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+  const accountsNames = accounts.map(account => account.name)
+  const accountsBalances = accounts.map(account => account.currentBalance)
   const data = {
-    datasets:[
+    datasets: [
       {
         label: "Banks",
-        data: [1250,2500,3750],
-        backgroundColor:["#0747b6","#2265d8","#2f91fa"]
+        data: accountsBalances,
+        backgroundColor: ["#0747b6", "#2265d8", "#2f91fa"]
       }
     ],
-    labels:["Bank 1","Bank 2","Bank 3"]
+    labels: accountsNames
   }
   return (
     <div>
-      <Doughnut 
+      <Doughnut
         data={data}
         options={{
           responsive: true,
