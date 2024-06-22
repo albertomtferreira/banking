be9447eb234de2b9e079fb50656e6fd5523c9997
Alberto Ferreira <albertomtferreira@gmail.com>
2024-06-22 16:18:59 +0100

    add force dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic"
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
@@ -12,7 +13,7 @@ const ibmPlexserif = IBM_Plex_Serif({
 export const metadata: Metadata = {
   title: "Horizon",
   description: "Horizon is a modern banking platform for everyone.",
-  icons:{
+  icons: {
     icon: "./icons/logo.svg"
   }
 };
